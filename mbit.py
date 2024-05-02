72ccf44e34cff4046b5e01c0eaa9f2dfb4b65e81
Lukkyye <97102436+lukkyye@users.noreply.github.com>
2024-05-02 19:09:59 +0000

    Add files via upload

---
+++ mbit.py
@@ -0,0 +1,27 @@
+import re
+import serial
+
+
+class tools():
+    def __init__(self, baudrate: int, port: int, data_regex = re.compile(r'-?\d+')):
+        self.port = port
+        self.baudrate = baudrate
+        self.regex = data_regex
+        
+    def connect(self) -> serial.Serial:
+        status = True
+        try:
+            connection = serial.Serial(self.baudrate, self.port)
+        except serial.SerialException:
+            status = False
+        return (status, connection)
+    
+    @classmethod
+    def getraw(cls, connection: serial.Serial)->str:
+        return connection.readline().decode().strip()
+    
+    def get(self, connection: serial.Serial)->str:
+        return self.regex.findall(tools.getraw(connection))
+    
+    def send(self, message, connection: serial.SerialBase):
+        connection.writelines(message)
